40841b73ca37d4609f990d96846c4c8f2d3c6a3d
Lop <lop@lorenzo.lophome.com>
2015-10-22 17:48:25 -0400

    added guiManager and WindowLogin/Dash classes

---
+++ src/com/growcontrol/lab/windows/WindowDash.java
@@ -0,0 +1,22 @@
+package com.growcontrol.lab.windows;
+
+import com.poixson.commonapp.gui.xWindow;
+import com.poixson.commonapp.gui.annotations.xWindowProperties;
+
+
+@xWindowProperties(
+		resizable = true
+)
+public class WindowDash extends xWindow {
+	private static final long serialVersionUID = 1L;
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/com/growcontrol/lab/windows/WindowLogin.java
@@ -0,0 +1,23 @@
+package com.growcontrol.lab.windows;
+
+import com.poixson.commonapp.gui.xWindow;
+import com.poixson.commonapp.gui.annotations.xWindowProperties;
+
+
+@xWindowProperties(
+		resizable = false
+)
+public class WindowLogin extends xWindow {
+	private static final long serialVersionUID = 1L;
+
+
+
+
+
+
+
+
+
+
+
+}
